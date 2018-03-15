2c4c96829c8fd4a01901e2f0e5215f408d04ad15
Marcelo Narvaez <marcelo_memo@hotmail.com>
2018-03-14 22:40:59 -0500

    Correcion del codigo
    Correon y verificacion del funcionamiento de codigo

--- server/getEvents.php
+++ server/getEvents.php
@@ -1,13 +1,14 @@
 <?php
   
 	session_start();
+
 	require('conexion.php');
 
 	if ($conn->connect_error) {
 		die("Conexión fallida: " . $conn->connect_error);
 	}else{
 
-		$query = "SELECT * FROM eventos WHERE id_usuario =".$_SESSION["_id_user"];
+		$query = "SELECT * FROM eventos WHERE id_usuario =" . $_SESSION["_id_user"];
 		$resultado = $conn->query($query);
 
 		if ($resultado->num_rows > 0) {
@@ -16,7 +17,7 @@
 				$evento["id"] = $reg["id"];
 				$evento["title"] = $reg["titulo"];
 				$evento["start"] = $reg["fecha_inicio"] . " " . $reg["hora_inicio"];
-				$evento["end"] = $reg["fecha_finalizacion"] . " " . $reg["hora_finalazacion"];
+				$evento["end"] = $reg["fecha_finalizacion"] . " " . $reg["hora_finalizacion"];
 				if ($reg["dia_completo"] == 1){
 					$evento["allDay"] = true;
 				}else{
@@ -26,7 +27,7 @@
 			}
 			$respuesta = array('msg' => 'OK', 'eventos' => $Eventos);
 		} else {
-		    $respuesta = array('msg' => 'No tienen eventos cargado'.$_SESSION["_id_user"]);
+		    $respuesta = array('msg' => 'No tienen eventos cargado' . $_SESSION["_id_user"]);
 		}
 	}
 
